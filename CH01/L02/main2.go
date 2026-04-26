d100c9f2ca3d72992f0af1d218ddf46ae16f1991
sharipovr <sharipov.rustem@gmail.com>
2026-04-25 22:58:42 -0500

    init: Completed Ch01

---
+++ CH01/L02/main2.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	var smsSendingLimit int
+	smsSendingLimit = 1000
+	fmt.Println("Your SMS sending limit is", smsSendingLimit)
+}
---
+++ CH01/L15/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	averageOpenRate, displayMessage := 0.23, "is the average open rate of your messages"
+	fmt.Println(averageOpenRate, displayMessage)
+}
